de282af09b078dd39bbe0ed89dd227a7c116c7d9
Pandora <imaginaqueumghoul@gmail.com>
2023-10-29 19:24:03 +0000

    testing gitignore

--- controllers/url.js
+++ controllers/url.js
@@ -10,6 +10,7 @@ async function generateNewShortURL(req, res) {
 
     const shortID = shortid();
     const db = await connectToMongoDB();
+    
     try {
         const urlCollection = db.collection('url');
     
