254870c4fee52ec140bccade7d26fd3fb414ecbb
Pandora <imaginaqueumghoul@gmail.com>
2023-10-30 01:11:42 +0000

    url ejs

--- index.js
+++ index.js
@@ -16,12 +16,16 @@ app.get('/', function (req, res) {
 
 app.get('/:shortID', async (req, res) => {
     const shortID = req.params.shortID;
+    if (shortID.length < 2) {
+        return
+    } 
     var link = await urlController.findRedirectURL(shortID);
     res.redirect(link)
 });
 
 app.post('/url', async (req, res) => {
-    urlController.generateNewShortURL(req, res);
+    let linkUrl = urlController.generateNewShortURL(req, res);
+    res.render(__dirname + 'views/pages/url.ejs', { linkURL: linkUrl} );
 });
 
 app.listen(8801, () => console.log("started"))
